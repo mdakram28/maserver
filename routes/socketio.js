8568d04d1f91c613339476fd0720e498e0d556eb
Mohd Akram Ansari <mdakram28@gmail.com>
2018-03-31 19:18:26 +0530

    Bleh

--- routes/socketio.js
+++ routes/socketio.js
@@ -1,25 +1,25 @@
-// var Gpio = require("onoff").Gpio;
-// var sellPin = new Gpio(17, 'out');
-// var buyPin = new Gpio(18, 'out');
+var Gpio = require("onoff").Gpio;
+var sellPin = new Gpio(17, 'out');
+var buyPin = new Gpio(18, 'out');
 
 module.exports = function(io) {
 	io.on("connection", socket => {
 		console.log("New Socket connected : ", socket.id);
 		
 		socket.on("start_selling", () => {
-			// sellPin.write(1, err => {if(err)console.log(err);});
+			sellPin.write(1, err => {if(err)console.log(err);});
 		});
 
 		socket.on("stop_selling", () => {
-			// sellPin.write(0, err => {if(err)console.log(err);});
+			sellPin.write(0, err => {if(err)console.log(err);});
 		});
 
 		socket.on("start_buying", () => {
-			// buyPin.write(1, err => {if(err)console.log(err);});
+			buyPin.write(1, err => {if(err)console.log(err);});
 		});
 
 		socket.on("stop_buying", () => {
-			// buyPin.write(0, err => {if(err)console.log(err);});
+			buyPin.write(0, err => {if(err)console.log(err);});
 		});
 
 	});
