5f44d5aad06d6f44555a70a59cf61301c1b68b95
Mohd Akram Ansari <mdakram28@gmail.com>
2018-03-31 19:21:43 +0530

    Bleh

--- routes/socketio.js
+++ routes/socketio.js
@@ -1,25 +1,30 @@
-var Gpio = require("onoff").Gpio;
-var sellPin = new Gpio(17, 'out');
-var buyPin = new Gpio(18, 'out');
+var prod = true;
+try {
+	var Gpio = require("onoff").Gpio;
+	var sellPin = new Gpio(17, 'out');
+	var buyPin = new Gpio(18, 'out');
+} catch (err) {
+	prod = false;
+}
 
-module.exports = function(io) {
+module.exports = function (io) {
 	io.on("connection", socket => {
 		console.log("New Socket connected : ", socket.id);
-		
+
 		socket.on("start_selling", () => {
-			sellPin.write(1, err => {if(err)console.log(err);});
+			if(prod)sellPin.write(1, err => { if (err) console.log(err); });
 		});
 
 		socket.on("stop_selling", () => {
-			sellPin.write(0, err => {if(err)console.log(err);});
+			if(prod)sellPin.write(0, err => { if (err) console.log(err); });
 		});
 
 		socket.on("start_buying", () => {
-			buyPin.write(1, err => {if(err)console.log(err);});
+			if(prod)buyPin.write(1, err => { if (err) console.log(err); });
 		});
 
 		socket.on("stop_buying", () => {
-			buyPin.write(0, err => {if(err)console.log(err);});
+			if(prod)buyPin.write(0, err => { if (err) console.log(err); });
 		});
 
 	});
