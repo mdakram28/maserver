7bc30e95c061d64a2753b39c251075355e82f4b2
Mohd Akram Ansari <mdakram28@gmail.com>
2018-03-31 23:55:31 +0530

    Bleh

--- routes/socketio.js
+++ routes/socketio.js
@@ -49,6 +49,7 @@ function init() {
 
 	function updatePins() {
 		if (isPi) {
+			console.log(state[id].selling, state[id].buying, state[id].forwarding);
 			sellPin.write(state[id].selling ? 1 : 0, err => { if (err) console.log(err); });
 			buyPin.write(state[id].buying ? 1 : 0, err => { if (err) console.log(err); });
 			forwardPin.write(state[id].forwarding ? 1 : 0, err => { if (err) console.log(err); });
